8dbae9ea144c4edb098f520f5f54edc5da154a7c
Askupa Software <contact@askupasoftware.com>
2016-06-23 23:24:25 -0500

    Moved Mivhak.render

--- src/js/mivhak.js
+++ src/js/mivhak.js
@@ -22,40 +22,6 @@ Mivhak.methodExists = function( method )
 
 /**
  * 
- * @param {type} name
- * @param {type} props
- */
-Mivhak.render = function(name, props)
-{
-    var component = $.extend(true, {}, Mivhak.components[name]);
-    var el = {};
-    
-    // Create the element from the template
-    el.$el = $(component.template);
-    
-    // Create all methods
-    $.each(component.methods, function(name, method){
-        el[name] = function() {return method.apply(el,arguments);};
-    });
-    
-    // Set properties
-    $.each(component.props, function(name, prop){
-        el[name] = (typeof props !== 'undefined' && props.hasOwnProperty(name) ? props[name] : prop);
-    });
-    
-    // Bind events
-    $.each(component.events, function(name, method){
-        el.$el.on(name, function() {return method.apply(el,arguments);});
-    });
-    
-    // Call the 'created' function if exists
-    if(component.hasOwnProperty('created')) component.created.call(el);
-    
-    return el;
-};
-
-/**
- * 
  */
 Mivhak.prototype.init = function() 
 {
